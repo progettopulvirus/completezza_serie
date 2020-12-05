56dbfa01e0518045cc868e6da9ea41bf09d06d91
guidofioravanti <guidofioravanti@yahoo.it>
2020-12-05 22:47:06 +0100

    Mappa delle stazioni complete di NO2

---
+++ mappa_stazioni_complete/R/mappe_geojson.R
@@ -0,0 +1,24 @@
+rm(list=objects())
+library("tidyverse")
+library("geojsonio")
+
+PARAM<-"NO2"
+
+read_delim("ana.csv",delim=";",col_names = TRUE)->ana
+
+
+list.files(pattern="^completezza.+csv$")->ffile
+
+purrr::map_dfr(ffile,.f=function(nomeFile){ 
+  
+  read_delim(nomeFile,delim=";",col_names=TRUE)->dati
+  
+  ana %>%
+    filter(station_eu_code %in% names(dati))
+
+  
+})->anagraficaStazioniComplete
+
+st_as_sf(anagraficaStazioniComplete,coords = c("st_x","st_y"),crs=4326)->sfAna
+
+geojsonio::geojson_write(sfAna,file=glue::glue("stazioniComplete_{PARAM}.geojson"))
